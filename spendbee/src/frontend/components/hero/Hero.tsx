1ab0f76842eb2e758c0755f86763fa5fa843536f
mh1803 <mhussain18m@gmail.com>
2025-12-10 21:19:59 +0000

    Added intial hero section

---
+++ spendbee/src/frontend/components/hero/Hero.tsx
@@ -0,0 +1,22 @@
+import styles from "./Hero.module.css";
+
+export function Hero() {
+  return (
+    <section id="home" className={styles.hero}>
+      <div className={styles.greeting}>
+        <h1 className={styles.title}>Take Control of Your Money</h1>
+        <h2 className={styles.subtitle}>
+          SpendBee: Effortless Bank Statement Analysis
+        </h2>
+        <p className={styles.description}>
+          Instantly understand your spending habits, track your expenses, and
+          make smarter financial decisions.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.primaryBtn}>Get Started for Free</button>
+          <button className={styles.secondaryBtn}>Learn How It Works</button>
+        </div>
+      </div>
+    </section>
+  );
+}
